e3d479dffdd545564b223c01a6dc42ff232bc833
Victor Ryan <vitorsilva3612@gmail.com>
2023-10-26 17:25:17 -0300

    Menu Inicial parte 1
    Cópia de https://github.com/VictorRyan3612/POO_mini_projeto

--- flutter_crud_app/lib/main.dart
+++ flutter_crud_app/lib/main.dart
@@ -1,19 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends HookWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    final currentBrightness = useState(Brightness.dark);
+    
+    final darkTheme = ThemeData(
+      brightness: Brightness.dark,
+      scaffoldBackgroundColor: const Color.fromARGB(255, 27, 27, 27)
+    );
+    final lightTheme = ThemeData(
+      brightness: Brightness.light,
+      primarySwatch: Colors.blue,
+      scaffoldBackgroundColor: const Color.fromARGB(255, 175, 175, 175)
+    );
+    return MaterialApp(
+      debugShowCheckedModeBanner:false,
+      
+      theme: currentBrightness.value == Brightness.dark ? darkTheme : lightTheme,
+
+      
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const Home(),
+      }
+    );
+      
+  }
+}
+
+class Home extends StatelessWidget{
+  const Home({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Hello World")
       ),
     );
   }
