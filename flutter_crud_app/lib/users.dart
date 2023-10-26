6ad0cb8cf2caad28c2850122efc1feea4e136a4c
Victor Ryan <vitorsilva3612@gmail.com>
2023-10-26 20:55:53 -0300

    Cadastro parte 1 tela

--- flutter_crud_app/lib/users.dart
+++ flutter_crud_app/lib/users.dart
@@ -1,17 +1,59 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
 
 
-class UserCadastro extends StatelessWidget{
+class Usuario {
+  final String nome;
+  final String email;
+
+  Usuario({required this.nome, required this.email});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'title': nome,
+      'email': email,
+    };
+  }
+
+  String toJson() => json.encode(toMap());
+}
+
+class UserCadastro extends StatefulWidget{
   const UserCadastro({super.key});
+
+  @override
+  State<UserCadastro> createState() => _UserCadastroState();
+}
+
+class _UserCadastroState extends State<UserCadastro> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Cadastro")
+        title: const Text("Cadastrar Usuario")
       ),
-      body: const Center(
-        child: Text("Cadastrar")
-      ),
+      body: const Padding(
+        padding: EdgeInsets.all(50.0),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Nome',
+                ),
+              ),
+              SizedBox(height: 10.0),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'email',
+                ),
+              ),
+            ]
+          ),
+        )
+      )
     );
   }
 }
