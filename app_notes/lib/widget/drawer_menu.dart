635701815b12650bc7e5d01e5678aa07bfde7b18
Victor Ryan <vitorsilva3612@gmail.com>
2023-11-19 19:38:37 -0300

    DrawerMenu parte 2
    Adaptado de prototipo:
    https://www.youtube.com/watch?v=eWwI5wtHEkQ

--- app_notes/lib/widget/drawer_menu.dart
+++ app_notes/lib/widget/drawer_menu.dart
@@ -1,23 +1,52 @@
 import 'package:flutter/material.dart';
 
+
 class DrawerMenu extends StatelessWidget {
   const DrawerMenu({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return Drawer(
+    bool isMobile = true;
+    return Container(
+      width: 300,
       child: ListView(
         
         padding: EdgeInsets.zero,
         children: [
-          const DrawerHeader(
-            decoration: BoxDecoration(
-              color: Colors.blue,
+          DrawerHeader(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+                colors: [
+                  Color.fromARGB(255, 80, 212, 252),
+                  Color.fromARGB(255, 124, 3, 222)
+                ]
+              )
             ),
-            child: Text('Drawer Header'),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                if (isMobile)
+                  IconButton(
+                    tooltip: "Voltar",
+                    icon: Icon(Icons.menu, 
+                      size: 30
+                    ),
+                    onPressed: () => Navigator.of(context).pop(), 
+                  ),
+                Padding(
+                  padding: EdgeInsets.only(left: 24),
+                  child: Text(
+                    'Drawer Header',
+                    style: TextStyle(fontSize: 25)
+                  ),
+                ),
+              ],
+            ),
           ),
           ListTile(
-            title: const Text('Item 1'),
+            title: const Text('All Notes'),
             onTap: () {
               // Update the state of the app.
               // ...
@@ -25,7 +54,16 @@ class DrawerMenu extends StatelessWidget {
           ),
 
           ListTile(
-            title: const Text('Item 2'),
+            title: const Text('Trash'),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+            },
+          ),
+          
+          const Divider(),
+          ListTile(
+            title: const Text('Settings'),
             onTap: () {
               // Update the state of the app.
               // ...
