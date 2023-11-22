417b0947a4d6b9bb1e0ed48f1a08e1ed67ca4c0a
Victor Ryan <vitorsilva3612@gmail.com>
2023-11-22 00:30:23 -0300

    Note detail parte 2.2 edicao

--- app_notes/lib/screen/note_detail.dart
+++ app_notes/lib/screen/note_detail.dart
@@ -1,19 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
 import '../data/note_data_service.dart';
 
 
-class NoteDetail extends StatelessWidget {
+class NoteDetail extends HookWidget {
+  
   const NoteDetail({super.key});
 
   @override
   Widget build(BuildContext context) {
-    var noteActual = noteDataService.aNoteValueNotifier.value;
+    var noteActual = noteDataService.aNoteValueNotifier.value[0];
+    final titleController= useTextEditingController(text: noteActual?.title ?? '');
+    final contentController= useTextEditingController(text: noteActual?.content ?? '');
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: noteActual.selectColor(noteActual),
-        title: Text(noteActual.title),
+        
+        title: TextField(
+          controller: titleController,
+          style: TextStyle(fontSize: 20),
+          onChanged: (value) {
+            noteActual.title = value;
+            noteDataService.aNoteValueNotifier.value[0].title = noteActual.title;
+            
+          },
+        ),
+
         leading: IconButton(
-          onPressed: () => Navigator.pop(context),
+          onPressed: () {
+            noteDataService.saveEditedNote(
+              editedNote: noteDataService.aNoteValueNotifier.value[0], 
+              index: noteDataService.aNoteValueNotifier.value[1]);
+            Navigator.pop(context);
+          },
           icon: Icon(
             Icons.arrow_back, 
             color: Colors.black
@@ -21,10 +42,21 @@ class NoteDetail extends StatelessWidget {
         ),
       ),
 
-      body: Center(
-        child: Text(noteActual.content)
+      body: Padding(
+        padding: const EdgeInsets.all(40.0),
+        child: TextField(
+          controller: contentController,
+          autofocus: true,
+          style: TextStyle(fontSize: 20),
+          maxLines: null,
+
+          onChanged: (value) {
+            noteActual.content = value;
+            noteDataService.aNoteValueNotifier.value[0].content = noteActual.content;
+            
+          },
+        ),
       ),
     );
-    // return Text(noteDataService.aNoteValueNotifier.value.title);
   }
 }
