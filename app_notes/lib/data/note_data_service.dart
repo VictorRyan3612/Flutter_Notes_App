ff6123e55cf6055d1a2ccda43a2cff7e3f47c42e
Victor Ryan <vitorsilva3612@gmail.com>
2024-03-06 08:15:35 -0300

    new fields, conversion old to new notes

--- app_notes/lib/data/note_data_service.dart
+++ app_notes/lib/data/note_data_service.dart
@@ -13,24 +13,31 @@ enum TableStatus{idle,loading,ready,error}
 class Note {
   late String title;
   late String content;
-  late String colorNote;
   late String tag;
+  late String colorNote;
+  late String dateCreate;
+  late String dateModified;
   late String status;
 
   Note(
-    {required this.title,
+    {
+    required this.title,
     required this.content,
-    this.colorNote = 'Red',
     this.tag = '',
-    this.status = "v"}
+    this.colorNote = 'Red',
+    this.dateCreate = '',
+    this.dateModified = '',
+    this.status = 'v'}
   );
 
   Map<String, dynamic> toMap() {
     return {
       'title': title,
       'content': content,
-      'colorNote': colorNote,
       'tag': tag,
+      'colorNote': colorNote,
+      'dateCreate': dateCreate,
+      'dateModified': dateModified,
       'status': status
     };
   }
@@ -59,16 +66,25 @@ class NoteDataService {
   final ValueNotifier<List> aNoteValueNotifier = ValueNotifier(
     [
       Note(
-        title: '', content: ''
+        title: '',
+        content: ''
       ),
-      int
+      int // index
     ]
   );
 
-  List<String> notesFieldsSortables = ['title', 'content', 'tag', 'colorNote'];
+  List<String> notesFieldsSortables = ['title','content', 'tag', 'colorNote', 'dateCreate', 'dateModified'];
   List<Note> originalList = [];
   bool isSorted = false;
   
+  Map<String, dynamic> checkFields(Map<String, dynamic> json){
+    for (String field in notesFieldsSortables) {
+      if (!json.containsKey(field)) {
+        json[field] = '';
+      }
+    }
+    return json;
+  }
   Future<List<Note>> loadNotesFromFile() async {
     Directory directory = await getApplicationSupportDirectory();
     File file = File('${directory.path}/notes.dat');
@@ -78,14 +94,19 @@ class NoteDataService {
       if (content != '') {
         List<dynamic> jsonList = json.decode(content);
         List<Note> notesList = jsonList.map((json) {
+          json = checkFields(json);
+
           return Note(
           title: json['title'],
           content: json['content'],
-          colorNote: json['colorNote'],
           tag: json['tag'],
+          colorNote: json['colorNote'],
+          dateCreate: json['dateCreate'],
+          dateModified: json['dateModified'],
           status: json['status'],
         );
         }).toList();
+
         return notesList;
       }
     }
@@ -95,12 +116,13 @@ class NoteDataService {
 
 
   void loadNotes() async {
-    var json = await loadNotesFromFile();
+    List<Note> listNotesLoaded = await loadNotesFromFile();
     notesValueNotifier.value = {
       'status':TableStatus.ready,
-      'dataObjects': json
+      'dataObjects': listNotesLoaded
     };
-    originalList = json;
+    originalList = listNotesLoaded;
+    saveNotesFile(listNotesLoaded);
   }
 
   defContent({required Note note, required int index}){
@@ -108,7 +130,7 @@ class NoteDataService {
     aNoteValueNotifier.value[1] = index;
   }
 
-  Future<void> saveNotes(List<Note> notes) async {
+  Future<void> saveNotesFile(List<Note> notes) async {
     Directory directory = await getApplicationSupportDirectory();
     File file = File('${directory.path}/notes.dat');
     String content = json.encode(notes.map((note) => note.toMap()).toList());
@@ -120,13 +142,13 @@ class NoteDataService {
     notesValueNotifier.value['dataObjects'] = [...notesValueNotifier.value['dataObjects'], newNote];
     notesValueNotifier.value['dataObjects'] = List<Note>.from(notesValueNotifier.value['dataObjects']);
 
-    saveNotes(notesValueNotifier.value['dataObjects']);
+    saveNotesFile(notesValueNotifier.value['dataObjects']);
     loadNotes();
   }
 
   saveEditedNote({required Note editedNote, required int index}){
     notesValueNotifier.value['dataObjects'][index] = editedNote;
-    saveNotes(notesValueNotifier.value['dataObjects']);
+    saveNotesFile(notesValueNotifier.value['dataObjects']);
     loadNotes();
   }
 
@@ -198,7 +220,7 @@ class NoteDataService {
   }
   void deleteNote(Note note) {
     note.status = 'x';
-    saveNotes(notesValueNotifier.value['dataObjects']);
+    saveNotesFile(notesValueNotifier.value['dataObjects']);
     loadNotes();
   }
 }
