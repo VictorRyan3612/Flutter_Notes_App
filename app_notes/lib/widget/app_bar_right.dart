4c51b50e8eb83ab9d9b7d5f9bedef7bd9c9a0b2b
Victor Ryan <vitorsilva3612@gmail.com>
2024-03-07 17:46:31 -0300

    AppBarRight botao mobile parte 1

--- app_notes/lib/widget/app_bar_right.dart
+++ app_notes/lib/widget/app_bar_right.dart
@@ -3,13 +3,25 @@ import 'package:app_notes/data/note_data_service.dart';
 import 'package:flutter/material.dart';
 
 
-class AppBarRight extends StatelessWidget {
+class AppBarRight extends StatelessWidget implements PreferredSizeWidget {
   const AppBarRight({super.key});
+
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      leading: IconButton(
+      leading: settingsService.isMobile.value ? IconButton(
+          onPressed: () {
+          },
+          icon: Icon(
+            Icons.arrow_back, 
+          )
+        )
+      : IconButton(
         tooltip: "Ativar/Desativar Lista de Notas",
         onPressed: (){
           settingsService.desktopLoadView.value = !settingsService.desktopLoadView.value;
