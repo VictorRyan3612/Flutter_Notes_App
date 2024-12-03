6888f077faf466f7c6d382779ad553fd0821021f
Victor Ryan <vitorsilva3612@gmail.com>
2024-12-03 11:51:19 -0300

    feat: wrapped ListTile with ConstrainedBox

--- app_notes/lib/view/list_notes.dart
+++ app_notes/lib/view/list_notes.dart
@@ -10,31 +10,41 @@ class ListNotes extends StatelessWidget{
   const ListNotes({super.key, required this.note, required this.index, required this.callbackClickFunction});
 
   @override
-  Widget build(BuildContext context) {
 
+  Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(5.0),
       child: InkWell(
         onTap: () async {
           callbackClickFunction(note, index);
         },
-        child: ListTile(
-          contentPadding: EdgeInsets.only(left: 0),
-          leading: Container(
-            color: note.selectColor(),
-            height: double.infinity,
-            width: 5,
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+            maxHeight: 80, // Limita a altura máxima
           ),
-          minLeadingWidth: 10,
-          
-          title: Text(
-            note.title,
-            style: TextStyle(
-              fontSize: 20, 
-              fontWeight: FontWeight.bold
-            )
+          child: ListTile(
+            contentPadding: EdgeInsets.only(left: 0),
+            leading: Container(
+              color: note.selectColor(),
+              height: double.infinity,
+              width: 5,
+            ),
+            minLeadingWidth: 10,
+            title: Text(
+              note.title,
+              style: TextStyle(
+                fontSize: 20, 
+                fontWeight: FontWeight.bold,
+              ),
+              maxLines: 1, // Limita o número de linhas do título
+              overflow: TextOverflow.ellipsis, // Adiciona "..." no final se ultrapassar
+            ),
+            subtitle: Text(
+              noteDataService.defSubtitle(note, '   '),
+              maxLines: 2, // Limita o número de linhas do subtítulo
+              overflow: TextOverflow.ellipsis,
+            ),
           ),
-          subtitle: Text(noteDataService.defSubtitle(note, '   ')),
         ),
       ),
     );
