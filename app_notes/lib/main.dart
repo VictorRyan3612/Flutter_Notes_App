11d8aa71a143bccedc3526800a3fe10503d26284
Victor Ryan <vitorsilva3612@gmail.com>
2024-12-04 15:11:29 -0300

    feat: use of VicrMaterialApp

--- app_notes/lib/main.dart
+++ app_notes/lib/main.dart
@@ -1,18 +1,19 @@
 // flutter packages
+import 'package:app_notes/config/settings_data_service.dart';
 import 'package:app_notes/config/shortcuts_settings.dart';
 import 'package:app_notes/data/note_data_service.dart';
+import 'package:app_notes/screen/desktop_home_screen.dart';
+import 'package:app_notes/screen/mobile_home_screen.dart';
 import 'package:app_notes/screen/note_detail.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_vicr_widgets/flutter_vicr_widgets.dart';
 
-
-import 'package:app_notes/layout/layout_decider.dart';
-import 'package:app_notes/config/theme_config.dart';
-import 'package:app_notes/screen/config_screen.dart';
 
 
 void main() {
+  VicrMaterialApp().loadSettings();
+  noteDataService.loadNotes();
   runApp(const MainApp());
 }
 
@@ -21,47 +22,18 @@ class MainApp extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    // base states 
-    final currentIsDarkMode = useState(true); //Theme
-    final currentColor = useState('Blue'); // Accent color
-
-    Future<void> loadSettings() async {
-      final prefs = await SharedPreferences.getInstance();
-      
-      final isDarkMode = prefs.getBool('isDarkMode') ?? true;
-      currentIsDarkMode.value = isDarkMode;
-
-      final colorTheme = prefs.getString('colorTheme') ?? 'Blue';
-      currentColor.value = colorTheme;
-
-    }
-
-    loadSettings();
-    noteDataService.loadNotes();
-    
-    final finalTheme = setTheme(currentIsDarkMode.value, currentColor.value);
-
-
     return CallbackShortcuts(
       bindings: ShortcutsSettings.getGlobalShortcuts(),
-      child: MaterialApp(
-        theme: finalTheme,
-        debugShowCheckedModeBanner: false,
-      
-      
-        initialRoute: '/',
+      child: VicrMaterialApp(
         routes: {
-          '/': (context) => LayoutDecider(),
-          '/noteDetail': (context) => NoteDetail(),
-          '/configs': (context) => ConfigScreen(
-            currentIsDarkMode: currentIsDarkMode,
-            currentColor: currentColor,
-            )
+          '/': (context) => VicrLayoutDecider(
+          isMobile: settingsService.isMobile,
+          optionMobile: MobileHomeScreen(),
+          optionDesktop: DesktopHomeScreen(),
+        ),
+          '/noteDetail': (context) => NoteDetail(), 
         },
       ),
     );
   }
 }
-
-
